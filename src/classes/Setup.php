dfc3888271404b44ca301d0117058d15cbadec8c
Oleksiy Boyda <oleksiyboyda@Oleksiys-MacBook-Pro.local>
2022-11-26 00:37:54 +0100

    fix

--- src/classes/Setup.php
+++ src/classes/Setup.php
@@ -104,6 +104,8 @@ class Setup
     {
         foreach(wpseed_get_dir_files($this->base_dir . '/mods', true, false) as $dir)
         {
+            if(!is_dir($dir)) continue;
+            
             $mod_index_file = $dir . '/index.php';
             if(file_exists($mod_index_file))
             {
