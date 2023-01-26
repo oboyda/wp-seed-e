9358f267e11919123affdccf1076732f8b5cc669
Oleksiy Boyda <oleksiyboyda@Oleksiys-MacBook-Pro.local>
2023-01-26 20:44:26 +0100

    js mods added

---
+++ src/js/mods/UtilsBase.js
@@ -0,0 +1,69 @@
+export class UtilsBase 
+{
+    static isMobile()
+    {
+        return (window.innerWidth < 992);
+    }
+
+    static isDesktop()
+    {
+        return !this.isMobile();
+    }
+
+    static isset(val)
+    {
+        return (typeof val !== "undefined" && val !== null);
+    }
+
+    static parseNamePath(name)
+    {
+        return (name.indexOf(".") > -1) ? name.split(".") : name;
+    }
+
+    static parseNamePathBase(name)
+    {
+        const _name = this.parseNamePath(name);
+        if(Array.isArray(_name))
+        {
+            return {
+                keyPath: _name[_name.length-1],
+                basePath: _name.slice(0, _name.length-1).join("."),
+            }
+        }
+
+        return {
+            keyPath: "",
+            basePath: _name
+        }
+    }
+
+    static getObjectPath(path, obj, _obj=null)
+    {
+        if(!this.isset(_obj))
+        {
+            _obj = obj;
+        }
+
+        if(path === "")
+        {
+            return _obj;
+        }
+
+        if(Array.isArray(path))
+        {
+            path.forEach((_path) => {
+                _obj = this.getObjectPath(_path, obj, _obj);
+            });
+        }
+        else if(path.indexOf(".") > -1)
+        {
+            _obj = this.getObjectPath(path.split("."), obj, _obj);
+        }
+        else
+        {
+            _obj = (this.isset(_obj) && this.isset(_obj[path])) ? _obj[path] : null;
+        }
+
+        return _obj;
+    }
+}
---
+++ src/js/mods/ViewUpdater.js
@@ -0,0 +1,229 @@
+import { UtilsBase } from "./UtilsBase";
+
+export class ViewUpdater 
+{
+    constructor(view, configElems={}, configsDefault={})
+    {
+        this.setView(view);
+        this.setConfigElems(configElems);
+        this.setConfigsDefault(configsDefault);
+
+        // this.applyConfigs();
+    }
+
+    setView(view)
+    {
+        this.view = view;
+    }
+
+    setConfigElems(configElems)
+    {
+        if(!this.isset(this.elems))
+        {
+            this.elems = {};
+        }
+
+        Object.keys(configElems).forEach((name) => {
+            const selector = configElems[name];
+            this.elems[name] = this.view.find(selector);
+        });
+    }
+
+    setConfigsDefault(configsDefault)
+    {
+        if(!this.isset(configsDefault.view))
+        {
+            configsDefault.view = {
+                elemOn: true
+            };
+        }
+
+        this.configsDefault = configsDefault;
+        this.resetConfigs();
+
+        this.setConfigsInit();
+    }
+
+    setConfigsInit()
+    {
+        const configsInit = this.view.data("configs");
+
+        if(this.isset(configsInit))
+        {
+            Object.keys(configsInit).forEach((name) => {
+                this.setConfig(name, configsInit[name], false);
+            });
+        }
+    }
+
+    setConfigs(configs, apply=false)
+    {
+        this.configs = configs;
+
+        if(apply)
+        {
+            this.applyConfigs();
+        }
+    }
+
+    setConfig(name, config, apply=false)
+    {
+        /*
+        config = {
+            text: "",
+            html: "",
+            addClass: "",
+            removeClass: "",
+            dataAtts: {},
+            atts: {},
+            events: {},
+            elemOn: true
+        }
+        -------------------------
+        */
+
+        const _name = this.parseNamePathBase(name);
+        
+        if(_name.keyPath)
+        {
+            const _config = this.getObjectPath(_name.basePath, this.configs);
+
+            if(this.isset(_config))
+            {
+                _config[_name.keyPath] = config;
+            }
+        }
+        // else if(this.isset(this.configs[_name.basePath]))
+        else if(typeof this.configs[_name.basePath] !== "undefined")
+        {
+            this.configs[_name.basePath] = config;
+        }
+
+        if(apply)
+        {
+            this.applyConfig(this.getNamePathElemName(name));
+        }
+    }
+
+    resetConfigs()
+    {
+        // this.setConfigs(Object.assign({}, this.configsDefault));
+        this.setConfigs(JSON.parse(JSON.stringify(this.configsDefault)));
+    }
+
+    getConfig(name)
+    {
+        const config = this.getObjectPath(name, this.configs);
+        // const configDefault = this.getObjectPath(name, this.configsDefault);
+        // return this.isset(config) ? config : (this.isset(configDefault) ? configDefault : null);
+
+        return this.isset(config) ? config : null;
+    }
+
+    applyConfigs(names=null)
+    {
+        const _names = this.isset(names) ? names : Object.keys(this.configs);
+        _names.forEach((name) => {
+            this.applyConfig(name);
+        });
+    }
+
+    applyConfig(name)
+    {
+        const _name = this.parseNamePathBase(name);
+        const config = this.getConfig(name);
+        const configElem = (_name.elemName == "view") ? this.view : ((typeof this.elems[_name.elemName] !== "undefined") ? this.elems[_name.elemName] : null);
+
+        if(!(
+            this.isset(configElem)
+            // && this.isset(config) 
+        )){
+            return;
+        }
+
+        const _config = (_name.keyPath !== "") ? { [_name.keyPath]: config } : config;
+
+        Object.keys(_config).forEach((c) => {
+
+            switch(c)
+            {
+                case "text":
+                    configElem.text(_config[c]);
+                break;
+                case "html":
+                    if(typeof _config[c] === "string"){
+                        configElem.html(_config[c]);
+                    }else{
+                        configElem.append(_config[c]);
+                    }
+                break;
+                case "addClass":
+                    configElem.addClass(_config[c]);
+                case "removeClass":
+                    configElem.removeClass(_config[c]);
+                break;
+                case "dataAtts":
+                    Object.keys(_config[c]).forEach((d) => {
+                        configElem.attr(`data-${d}`, _config[c][d]);
+                    });
+                break;
+                case "atts":
+                    Object.keys(_config[c]).forEach((a) => {
+                        configElem.attr(a, _config[c][a]);
+                    });
+                break;
+                case "events":
+                    Object.keys(_config[c]).forEach((e) => {
+                        const callback = _config[c][e];
+                        if(typeof callback === "function")
+                        {
+                            configElem.off(e, callback);
+                            configElem.on(e, callback);
+                        }
+                    });
+                break;
+                case "elemOn":
+                    if(_config[c])
+                    {
+                        configElem.removeClass("d-none");
+                    }
+                    else{
+                        configElem.addClass("d-none");
+                    }
+                break;
+            }
+        });
+    }
+
+    isset(val)
+    {
+        return UtilsBase.isset(val);
+    }
+
+    parseNamePath(name)
+    {
+        return UtilsBase.parseNamePath(name);
+    }
+
+    parseNamePathBase(name)
+    {
+        const _name = UtilsBase.parseNamePathBase(name);
+
+        return {
+            keyPath: _name.keyPath,
+            basePath: _name.basePath,
+            elemName: this.getNamePathElemName(_name.basePath)
+        };
+    }
+
+    getNamePathElemName(name)
+    {
+        const _name = name.split(".");
+        return (typeof _name[0] !== "undefined") ? _name[0] : name;
+    }
+
+    getObjectPath(path, obj, _obj=null)
+    {
+        return UtilsBase.getObjectPath(path, obj, _obj);
+    }
+}
