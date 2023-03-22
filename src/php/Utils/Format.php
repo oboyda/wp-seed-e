32e302e8dc2854f31fe8c9c48ff53df50a42ab4f
Oleksiy Boyda <oleksiyboyda@Oleksiys-MacBook-Pro.local>
2023-03-22 18:43:15 +0100

    1.2.62

--- src/php/Utils/Format.php
+++ src/php/Utils/Format.php
@@ -9,10 +9,19 @@ class Format
         return function_exists('wc_price') ? wc_price($price) : $price;
     }
 
-    static function formatPhoneSys($phone)
+    static function formatPhoneSys($phone, $replace_plus=false)
     {
+        if($replace_plus && strpos($phone, '+') === 0){
+            $phone = '00' . substr($phone, 1);
+        }
+
         // return str_replace([' ', '-', '.'], '', $phone);
         // return preg_replace('/\d/', '', $phone);
-        return preg_replace('/[^0-9]/', '', $phone);
+        // return preg_replace('/[^0-9]/', '', $phone);
+
+        $phone = filter_var($phone, FILTER_SANITIZE_NUMBER_INT);
+        $phone = str_replace([' ', '-', '.'], '', $phone);
+
+        return $phone;
     }
 }
